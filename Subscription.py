18e2cab0b938862c610e9e174bd2df35294f3f8f
Steven Elkind <saelkind@users.noreply.github.com>
2023-07-14 13:36:26 -0400

    adding initial stubbed-out files

---
+++ Subscription.py
@@ -0,0 +1,9 @@
+
+
+class Subscription:
+
+    def __init__(self, subscription_rec: []):
+        self.firstname = None
+        self.lastname = None
+        self.email_address = None
+        self.subscription_list: () = None
