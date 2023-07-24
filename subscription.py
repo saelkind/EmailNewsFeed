a50fab47e302da0ef9a3dd906b3006f1590ce7f4
Steven Elkind <saelkind@users.noreply.github.com>
2023-07-24 15:43:59 -0400

    Minor change - refactor lengthy config params check into its own method for clarity.  Also to try out PyCharm refactor-to-method capability

--- subscription.py
+++ subscription.py
@@ -7,7 +7,7 @@ class Subscription:
     Convenience class with static methods for processing one subscription at a time.
     For any given subscription, it finds news items of interest to the subscriber,
     by connecting to the news source using the apinews.org API key,
-    and retrieves news articles for the subscription.
+    and retrieves news articles for the cls.
     It then builds and sends the email with the list in a nicely-formatted (hah!)
     manner.  Finally,, it sends the email to the subscriber
     """
@@ -29,28 +29,20 @@ class Subscription:
         :return: list with email sent OK, error msg if not, topics requested, topics processed, articles retrieved
         """
         logger = logging.getLogger(NewsSender.LOGGER_NAME)
-        firstname: str = this_subs_rec[Subscription.IDX_FIRSTNAME]
-        lastname: str = this_subs_rec[Subscription.IDX_LASTNAME]
-        email_address: str = this_subs_rec[Subscription.IDX_EMAIL]
-        subscription_list: list = this_subs_rec[Subscription.IDX_TOPIC_LIST]
+        firstname: str = this_subs_rec[cls.IDX_FIRSTNAME]
+        lastname: str = this_subs_rec[cls.IDX_LASTNAME]
+        email_address: str = this_subs_rec[cls.IDX_EMAIL]
+        subscription_list: list = this_subs_rec[cls.IDX_TOPIC_LIST]
         topics_requested = len(subscription_list)
-        topics_retrieved: dict = {}
-        topics_done = 0
-        articles_retrieved = 0
-        for topic in subscription_list:
-            if topics_done == newssender.max_topics_per_subscription:
-                # don't bother the API server for excess topics in this subscription
-                break
-            topics_retrieved[topic] = Subscription.get_news(topic, newssender)
-            topics_done += 1
-            articles_retrieved += len(topics_retrieved[topic]["articles"])
+        articles_retrieved, topics_done, topics_retrieved = \
+            cls.process_topics(newssender, subscription_list)
         # Hack alert!!!!! circular reference avoidance
         from email_content import EmailContent
         email_content = EmailContent(this_subs_rec, newssender)
         email_content.build_email_content(topics_retrieved)
         status_list = newssender.send_html_email("Your daily NewsFeed",
                                                  email_content.body,
-                                                 [this_subs_rec[Subscription.IDX_EMAIL]])
+                                                 [this_subs_rec[cls.IDX_EMAIL]])
         logger.info(f"{email_address}: done processing subscription")
         logger.info(f"{email_address}: {topics_done} completed out of {topics_requested} requested; "
                     f"{articles_retrieved} total articles retrieved")
@@ -59,6 +51,20 @@ class Subscription:
                 "topics_requested": topics_requested,
                 "topics_retrieved": len(topics_retrieved),
                 "articles_retrieved": articles_retrieved}
+
+    @classmethod
+    def process_topics(cls, newssender, subscription_list):
+        topics_retrieved: dict = {}
+        topics_done = 0
+        articles_retrieved = 0
+        for topic in subscription_list:
+            if topics_done == newssender.max_topics_per_subscription:
+                # don't bother the API server for excess topics in this subscription
+                break
+            topics_retrieved[topic] = cls.get_news(topic, newssender)
+            topics_done += 1
+            articles_retrieved += len(topics_retrieved[topic]["articles"])
+        return articles_retrieved, topics_done, topics_retrieved
 
     @classmethod
     def get_news(cls, topic: str, newssender: NewsSender) -> dict:
@@ -74,7 +80,7 @@ class Subscription:
         logger = logging.getLogger(NewsSender.LOGGER_NAME)
         sort_by = f"&sortBy={newssender.sort_order}"
         search_in = f"&searchin=description"
-        url = f"{Subscription.NEWS_API_BASE_URL}?q={topic}"
+        url = f"{cls.NEWS_API_BASE_URL}?q={topic}"
         url += f"&from={newssender.date_str}&language=en" \
               f"{search_in}{sort_by}" \
               f"&pageSize={newssender.max_articles_per_topic}" \
@@ -94,4 +100,4 @@ class Subscription:
 #         subscription_record = ["Steven", "Elkind", "selkind@aol.com",
 #                             "Tesla, Ford, Powerwall, Marshmallows, Whipped Cream, Butterscotch"]
 #         subscription = Subscription(news_sender, subscription_record)
-#         subscription.process_subscription()
+#         cls.process_subscription()
